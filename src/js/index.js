681da4118d06845962513937513f506591c5cfff
Orbmancer <damien.bry@gmail.com>
2018-01-29 11:02:53 +0100

    added micron.js interations :heart:

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,11 @@
-// This file is here to handle refreshes
-
 require('../scss/main.scss');
+
+const micron = require('webkul-micron');
+
+setTimeout(() => {
+  micron.getEle('.landing.index h2').interaction('tada').duration('.45').timing('ease-in-out');
+}, 500);
+
+setTimeout(() => {
+  micron.getEle('.landing:not(.index) h1').interaction('pop').duration('.45').timing('ease-in-out');
+}, 500);
